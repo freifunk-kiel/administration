794b34f72233b29381a0c1990b89b723793ef8cf
Ruben Barkow <rubo77@users.noreply.github.com>
2016-12-18 23:33:21 +0100

    Create autoupdater-forcer.sh

---
+++ autoupdater-forcer.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# call this script with an ipv6 or hostname of the router
+if [ "$1" = "" ]; then
+        echo call this script with an ipv6 or hostname of the router:
+        echo $0 '[IPv6]'
+        exit
+fi
+ssh root@$1 <<'ENDSSH'
+#!/bin/sh
+echo -n "### hostname :  "
+cat /proc/sys/kernel/hostname
+echo -n "### firmware :  "
+cat /lib/gluon/release
+echo -n "### hardware :  "
+cat /tmp/sysinfo/model
+echo "Das Client WLAN ausschalten..."
+uci set wireless.client_radio0.disabled=1
+uci commit
+uci set autoupdater.settings.branch='stable'
+uci set autoupdater.stable.good_signatures='1'
+uci commit
+autoupdater -f
+ENDSSH
